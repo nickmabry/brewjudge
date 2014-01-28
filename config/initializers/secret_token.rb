f78224ab31e9e37fd54eb2edc8da698d402887d1
Nick Mabry <nicholasmabry@gmail.com>
2014-01-27 23:25:13 -0700

    Empty Rails project with CI and index page created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwelveSpokes::Application.config.secret_key_base = 'd128e620ee96e91081d7c30ddf316126bf387a84d9d1c61dd254089e54cf084621c1b8850ad6d00c83682a6ca3cccb2d94054b3e883109b6bd398cca9442af16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwelveSpokes::Application.config.session_store :cookie_store, key: '_twelve_spokes_session'
